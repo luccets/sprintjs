c341ab5d433a4f1567b2da0a7d6f514cf4614506
luccets <lucca.vilaca@gmail.com>
2023-05-20 19:03:33 -0300

    sprint2js

---
+++ script.js
@@ -0,0 +1,30 @@
+// declarando as variaveis 
+
+let forms = document.getElementById("formsid")
+let nameinput = document.getElementById("nomeid")
+let telefoneinput = document.getElementById("telefoneid")
+let emailinput = document.getElementById("emailid")
+
+forms.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // verifica se o nome esta vazio 
+    if (nameinput.value === "") {
+        alert ("preencha seu nome");
+        return;
+    }
+
+    // verifica se o telefone esta vazio 
+    if (telefoneinput.value === "") {
+        alert ("preencha seu telefone");
+        return;
+    }
+    // verifica se o nome esta vazio 
+    if (emailinput.value === "") {
+        alert ("preencha seu email");
+        return;
+    }
+
+    // se estiver tudo preenchido 
+    forms.submit();
+})
